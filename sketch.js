da0f3471094e5234812f068ca717696cceff3313
anishka-agarwal <86351578+anishka-agarwal@users.noreply.github.com>
2021-08-07 13:14:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var marks = [35,38,42,45,43,34,46,41,48,32];
+
+
+function setup() 
+{
+  for (let i = 0; i < marks.length; i++) {
+   if (marks[i]>= 45) {
+     console.log(marks[i])
+
+
+   }
+    
+  }
+  createCanvas(400,400);
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
